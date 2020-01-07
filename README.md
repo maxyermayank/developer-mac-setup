274a720924cee31880f927f812d794e90669de5c
maxyermayank <maxy.ermayank@gmail.com>
2020-01-07 14:50:54 -0700

    Script Improvements

--- README.md
+++ README.md
@@ -20,36 +20,19 @@ chmod +x macSetup.sh
 ./macSetup.sh
 ```
 
-
-## **Install Homebrew & Cask**
-- [x] [Homebrew](https://brew.sh)
-```
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap caskroom/cask
-```
-
-## **Update Homebrew**
-```
-brew update
-```
-
-## **Install Mac Command Line Tools**
-```
-xcode-select --install
-```
+## **General Configurations & Aliases**
+- [x] Git Config
+- [x] Global Git Ignore
+- [x] Mac Screenshot Config
+- [x] Git Aliases
+- [x] Docker Aliases
+- [x] K8s Aliases
 
 ## **Install Browsers**
 - [x] [Google Chrome](https://www.google.com/chrome/)
 - [ ] [Google Chrome Canary](https://www.google.com/chrome/canary/)
 - [x] [Firefox](https://www.mozilla.org/en-US/firefox/new/)
 - [ ] [Firefox Developer Edition](https://www.mozilla.org/en-US/firefox/developer/)
-```
-brew cask install google-chrome
-brew cask install firefox
-# brew cask install google-chrome-canary
-# brew cask install firefox-developer-edition
-```
-
 
 ## **Install HashiCorp Tools**
 - [x] [Terraform](https://www.terraform.io)
@@ -58,27 +41,12 @@ brew cask install firefox
 - [x] [Packer](https://www.packer.io)
 - [x] [Nomad](https://www.nomadproject.io)
 - [x] [Vagrant](https://www.vagrantup.com)
-```
-brew install terraform
-brew install vault
-brew install consul
-brew install nomad
-brew install packer
-brew cask install vagrant
-```
-
 
 ## **Install Virtualization Tools**
 - [x] [VMWare Fusion](https://www.vmware.com/products/fusion.html)
 - [x] [Oracle Virtualbox](https://www.virtualbox.org)
 - [x] [Docker](https://www.docker.com)
 - [x] [Vagrant Manager](http://vagrantmanager.com)
-```
-brew cask install vmware-fusion
-brew cask install virtualbox
-brew cask install docker
-brew cask install vagrant-manager
-```
 
 
 ## **Install IDEs (Intellij, Visual Studio, Visual Studio Code, Android Studio)**
@@ -86,12 +54,7 @@ brew cask install vagrant-manager
 - [x] [Visual Studio](https://visualstudio.microsoft.com)
 - [x] [Visual Studio Code](https://code.visualstudio.com)
 - [x] [Android Studio](https://developer.android.com/studio)
-```
-brew cask install visual-studio-code
-brew cask install intellij-idea
-brew cask install visual-studio
-brew cask install android-studio
-```
+
 
 # **Install important Visual Studio Code Extensions**
 If you want to add any new extension besides all the ones listed below then update vscode-extensions.txt before executing script.
@@ -121,25 +84,12 @@ If you want to add any new extension besides all the ones listed below then upda
 - [x] [Git History](https://marketplace.visualstudio.com/items?itemName=donjayamanne.githistory)
 - [x] [AWS Toolkit for Visual Studio Code](https://marketplace.visualstudio.com/items?itemName=AmazonWebServices.aws-toolkit-vscode)
 - [x] [C#](https://marketplace.visualstudio.com/items?itemName=ms-vscode.csharp)
-```
-cat ./vscode-extensions.txt | xargs -L1 code --install-extension
-```
+
 
 ## **Install AWS Tools**
 - [x] [AWS CLI](https://aws.amazon.com/cli/)
 - [x] [SAM CLI](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html)
-```
-pip3 --version
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
-pip3 install awscli --upgrade --user
-aws --version
-rm get-pip.py
 
-brew tap aws/tap
-brew install aws-sam-cli
-sam --version
-```
 
 ## **Install Developer Utilities**
 - [x] [SDKMan](https://sdkman.io)
@@ -151,38 +101,15 @@ sam --version
 - [x] [httpie](https://httpie.org)
 - [x] [NVM](https://github.com/nvm-sh/nvm)
 - [x] [Bash Completion](https://github.com/scop/bash-completion)
-```
-curl -s "https://get.sdkman.io" | bash
-brew cask install spectacle
-brew install tree
-brew install ctop
-brew install jq
-brew cask install postman
-brew install httpie
-
-brew install nvm
-mkdir ~/.nvm
-## TO DO Add to .bash_profile
-
-brew install bash-completion
-## TO DO Add to .bash_profile
-```
 
 ## **Install Language SDKs/Platforms**
 - [x] [Dotnet SDK](https://dotnet.microsoft.com/download)
 - [x] [Go](https://golang.org)
-```
-brew cask install dotnet-sdk
-brew cask install go
-```
+
 
 ## **Install Database Tools**
 - [x] [PGAdmin](https://www.pgadmin.org)
 - [x] [Mongo Studio 3T](https://studio3t.com)
-```
-brew cask install pgadmin4
-brew cask install studio-3t
-```
 
 
 ## **Install Productivity Tools**
@@ -191,50 +118,7 @@ brew cask install studio-3t
 - [x] [The Unarchiver](https://theunarchiver.com)
 - [x] [CheatSheet](https://mediaatelier.com/CheatSheet/)
 - [x] [Adobe Creative Cloud](https://www.adobe.com/creativecloud.html)
-```
-brew cask install slack
-brew cask install evernote
-brew cask install the-unarchiver
-brew cask install cheatsheet
-brew cask install adobe-creative-cloud
-```
 
-
-## **Global Git Config**
-```
-git config --global push.default current
-git config --global core.excludesfile ~/.gitignore
-git config --global user.name "<username>"
-git config --global user.email <email>
-git config --global color.branch auto
-git config --global color.diff auto
-git config --global color.interactive auto
-git config --global color.status auto
-git config --global alias.st status
-git config --global alias.ci commit
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue <%an>%Creset' --abbrev-commit --date=relative"
-```
-
-## **Setup Docker Aliases**
-```
-alias dps='docker ps -a'
-alias di='docker images -a'
-alias dv='docker volume ls'
-alias dn='docker network ls'
-alias sprune='docker system prune'
-alias vprune='docker volume prune'
-```
-
-## **Docker Swarm Aliases**
-```
-alias dstr='docker stack rm'
-alias dsls='docker service ls'
-alias dsp='docker service ps'
-alias dsl='docker service logs'
-alias dsr='docker service rm'
-```
 
 # **Resources**
 - [MAC OSX BASH PROFILE](https://natelandau.com/my-mac-osx-bash_profile/)
--- macSetup.sh
+++ macSetup.sh
@@ -1,104 +1,300 @@
+
 #!/bin/bash
 
-## Install Homebrew & Cask
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap caskroom/cask
+start=`date +%s`
+bold=$(tput bold)
+normal=$(tput sgr0)
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
-## Update Homebrew
+if test ! $(which gcc); then
+  echo "Installing command line developer tools..."
+  xcode-select --install
+fi
+
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install caskroom/cask/brew-cask
+    brew tap caskroom/versions
+    brew tap caskroom/cask
+    brew tap 'homebrew/bundle'
+    brew tap 'homebrew/cask'
+    brew tap 'homebrew/cask-drivers'
+    brew tap 'homebrew/cask-fonts'
+    brew tap 'homebrew/core'
+    brew tap 'homebrew/services'
+    brew tap aws/tap
+
+fi
+
+echo "Updating homebrew..."
 brew update
-
-## Install Mac Command Line Tools
-xcode-select --install
-
-## Install Browsers (Google Chrome, Firefox)
-brew cask install google-chrome
-brew cask install firefox
-# brew cask install google-chrome-canary
-# brew cask install firefox-developer-edition
+brew upgrade
 
 
-## Install HashiCorp Tools (Terrafor, Vault, Consul, Packer & Vagrant)
-brew install terraform
-brew install vault
-brew install consul
-brew install nomad
-brew install packer
-brew cask install vagrant
+beginDeploy() {
+    echo
+    echo "${bold}$1${normal}"
+}
+
+############# General Tools #############
+beginDeploy "############# General Tools #############"
+echo -n "Do you wish to install General Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
+read General
+
+CaskGeneralToolList=(
+    google-chrome
+    firefox
+    spotify
+)
+if [ "$General" != "${General#[Yy]}" ] ;then
+    echo Yes
+    brew cask install --appdir="/Applications" ${CaskGeneralToolList[@]}
+else
+    echo No
+fi
+
+############# Designer #############
+beginDeploy "############# Designer #############"
+echo -n "Do you wish to install Designer Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
+read Designer
+
+CaskDesignerToolList=(
+    adobe-creative-cloud
+)
+if [ "$Designer" != "${Designer#[Yy]}" ] ;then
+    echo Yes
+    brew cask install --appdir="/Applications" ${CaskDesignerToolList[@]}
+else
+    echo No
+fi
 
 
-## Install Virtualization Tools (VMWare Fusion, Oracle Virtualbox, Docker, Vagrant Manager)
-brew cask install vmware-fusion
-brew cask install virtualbox
-brew cask install docker
-brew cask install vagrant-manager
+############# Mobile Developer #############
+beginDeploy "############# Mobile Developer #############"
+echo -n "Do you wish to install Mobile Developer Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
+read MobileDeveloper
+
+CaskMobileDeveloperToolList=(
+    fastlane
+)
+if [ "$MobileDeveloper" != "${MobileDeveloper#[Yy]}" ] ;then
+    echo Yes
+    brew cask install ${CaskMobileDeveloperToolList[@]}
+else
+    echo No
+fi
 
 
-## Install IDEs (Intellij, Visual Studio, Visual Studio Code, Android Studio)
-brew cask install visual-studio-code
-brew cask install intellij-idea
-brew cask install visual-studio
-brew cask install android-studio
+############# Developer Utilities #############
+beginDeploy "############# Developer Utilities #############"
+echo -n "Do you wish to install Developer Utilities (${bold}${green}y${reset}/${bold}${red}n${reset})? "
+read DeveloperUtilities
 
-# Install important Visual Studio Code Extensions
-cat vscode-extensions.txt | xargs -L1 code --install-extension
-
-## Install AWS Tools (AWS CLI & SAM CLI)
-pip3 --version
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
-pip3 install awscli --upgrade --user
-aws --version
-rm get-pip.py
-
-brew tap aws/tap
-brew install aws-sam-cli
-sam --version
-
-## Install Developer utilities (SDKMan, Spectacle, Tree, Ctop, JQ, Postman, httpie, NVM, Bash Completion)
-curl -s "https://get.sdkman.io" | bash
-echo '
-# THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="/Users/mpatel/.sdkman"
-[[ -s "/Users/mpatel/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/mpatel/.sdkman/bin/sdkman-init.sh"
-' >> ~/.bash_profile
-
-brew cask install spectacle
-brew install tree
-brew install ctop
-brew install jq
-brew cask install postman
-brew install httpie
-
-brew install nvm
-mkdir ~/.nvm
-echo '
-# NVM CONFIG
-export NVM_DIR="$HOME/.nvm"
-    [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && . "$(brew --prefix)/opt/nvm/nvm.sh" # This loads nvm
-    [ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && . "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion' >> ~/.bash_profile
-
-brew install bash-completion
-echo '
-# BASH-COMPLETION CONFIG
-[[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]] && . "$(brew --prefix)/etc/profile.d/bash_completion.sh"' >> ~/.bash_profile
-
-## Install Language SDKs/Platforms
-brew cask install dotnet-sdk
-brew cask install go
-
-## Install Database Tools (PGAdmin, Mongo Studio 3T)
-brew cask install pgadmin4
-brew cask install studio-3t
+DeveloperUtilitiesList=(
+    tree
+    ctop
+    jq
+    httpie
+    yarn
+    yarn-completion
+    netcat
+    nmap
+    wget
+    nvm
+    bash-completion
+)
+CaskDeveloperUtilitiesList=(
+    cheatsheet
+    spectacle
+    postman
+    dotnet-sdk
+    go
+    wireshark
+    # google-chrome-canary
+    # firefox-developer-edition
+)
+if [ "$DeveloperUtilities" != "${DeveloperUtilities#[Yy]}" ] ;then
+    
+    echo Yes
+    brew install ${DeveloperUtilitiesList[@]}
+    brew cask install ${CaskDeveloperUtilitiesList[@]}
 
 
-## Install Productivity Tools (Slack, Evernote, Unarchiver, CheatSheet, Adobe Creative Cloud)
-brew cask install slack
-brew cask install evernote
-brew cask install the-unarchiver
-brew cask install cheatsheet
-brew cask install adobe-creative-cloud
+    mkdir ~/.nvm
+    echo '
+    # NVM CONFIG
+    export NVM_DIR="$HOME/.nvm"
+        [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && . "$(brew --prefix)/opt/nvm/nvm.sh" # This loads nvm
+        [ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && . "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion' >> ~/.bash_profile
 
-## Global Git Config
+
+    echo '
+    # BASH-COMPLETION CONFIG
+    [[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]] && . "$(brew --prefix)/etc/profile.d/bash_completion.sh"' >> ~/.bash_profile
+
+
+    curl -s "https://get.sdkman.io" | bash
+    echo '
+    # THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+    export SDKMAN_DIR="/Users/mpatel/.sdkman"
+    [[ -s "/Users/mpatel/.sdkman/bin/sdkman-init.sh" ]] && source "${HOME}/.sdkman/bin/sdkman-init.sh"' >> ~/.bash_profile
+else
+    echo No
+fi
+
+
+############# Database Tools #############
+beginDeploy "############# Database Tools #############"
+echo -n "Do you wish to install Database Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
+read Database
+
+
+DatabaseToolList=(
+    kafkacat
+)
+CaskDatabaseToolList=(
+    pgadmin4
+    studio-3t
+    graphiql
+)
+if [ "$Database" != "${Database#[Yy]}" ] ;then
+    echo Yes
+    brew install ${DatabaseToolList[@]}
+    brew cask install ${CaskDatabaseToolList[@]}
+
+else
+    echo No
+fi
+
+
+############# IDEs #############
+beginDeploy "############# IDEs #############"
+echo -n "Do you wish to install IDEs (${bold}${green}y${reset}/${bold}${red}n${reset})? "
+read IDEs
+
+CaskIDEsList=(
+    visual-studio-code
+    intellij-idea
+    visual-studio
+    android-studio
+)
+if [ "$IDEs" != "${IDEs#[Yy]}" ] ;then
+    echo Yes
+    brew cask install --appdir="/Applications" ${CaskDatabaseToolList[@]}
+    cat vscode-extensions.txt | xargs -L1 code --install-extension
+else
+    echo No
+fi
+
+
+############# DevOps #############
+beginDeploy "############# DevOps #############"
+echo -n "Do you wish to install DevOps Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
+read DevOps
+
+
+DevOpsToolList=(
+    terraform
+    vault
+    consul
+    nomad
+    packer
+    terragrunt
+    ansible
+    aws-sam-cli
+    kompose
+)
+CaskDevOpsToolList=(
+    vagrant
+    vmware-fusion
+    virtualbox
+    docker
+    vagrant-manager
+)
+if [ "$DevOps" != "${DevOps#[Yy]}" ] ;then
+    echo Yes
+    brew install ${DatabaseToolList[@]}
+    brew cask install ${CaskDatabaseToolList[@]}
+
+    ## DOCKER APP
+    wget -P ~/Downloads/ https://github.com/docker/app/releases/download/v0.6.0/docker-app-darwin.tar.gz
+    tar -xvf ~/Downloads/docker-app-darwin.tar.gz -C ~/Downloads/
+    mv ~/Downloads/docker-app-darwin /usr/local/bin/docker-app
+    rm ~/Downloads/docker-app-darwin.tar.gz
+
+
+    ## Install AWS CLI
+    pip3 --version
+    curl -O https://bootstrap.pypa.io/get-pip.py
+    python3 get-pip.py --user
+    pip3 install awscli --upgrade --user
+    aws --version
+    rm get-pip.py
+else
+    echo No
+fi
+
+
+############# Productivity Tools #############
+beginDeploy "############# Productivity Tools #############"
+echo -n "Do you wish to install Productivity Tools (${bold}${green}y${reset}/${bold}${red}n${reset})? "
+read Productivity
+
+CaskProductivityToolList=(
+    slack
+    evernote
+    the-unarchiver
+    dash
+    gpg-suite
+    microsoft-teams
+    microsoft-office
+    zoomus
+)
+if [ "$Productivity" != "${Productivity#[Yy]}" ] ;then
+    echo Yes
+    brew cask install --appdir="/Applications" ${CaskProductivityToolList[@]}
+else
+    echo No
+fi
+
+
+############# Mac Application #############
+beginDeploy "############# Mac Application #############"
+echo -n "Do you wish to install Mac Application (${bold}${green}y${reset}/${bold}${red}n${reset})? "
+read MacApplication
+
+MacApplicationToolList=(
+    409183694 # Keynote
+    409203825 # Numbers
+    409201541 # Pages
+    497799835 # Xcode
+    1450874784 # Transporter
+    1274495053 # Microsoft To Do
+    1295203466 # Microsoft Remote Desktop 10
+    985367838 # Microsoft Outlook
+)
+if [ "$MacApplication" != "${MacApplication#[Yy]}" ] ;then
+    brew install mas
+    mas install ${MacApplicationToolList[@]}
+
+    echo "######### Save screenshots to ${HOME}/Pictures/Screenshots"
+    defaults write com.apple.screencapture location -string "${HOME}/Pictures/Screenshots"
+
+    echo "######### Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF"
+    defaults write com.apple.screencapture type -string "png"
+
+else
+    echo No
+fi
+
+beginDeploy "############# CLEANING HOMEBREW #############"
+brew cleanup
+
+beginDeploy "############# GLOBAL GIT CONFIG #############"
+sh -c 'curl -s https://raw.githubusercontent.com/maxyermayank/developer-mac-setup/master/.gitignore >> ~/.gitignore'
 git config --global push.default current
 git config --global core.excludesfile ~/.gitignore
 git config --global user.name "<username>"
@@ -107,30 +303,22 @@ git config --global color.branch auto
 git config --global color.diff auto
 git config --global color.interactive auto
 git config --global color.status auto
-git config --global alias.st status
-git config --global alias.ci commit
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 
-## Setup Docker Aliases
-echo "
-alias dps='docker ps -a'
-alias di='docker images -a'
-alias dv='docker volume ls'
-alias dn='docker network ls'
-alias sprune='docker system prune'
-alias vprune='docker volume prune'
+beginDeploy "############# ALIASES #############"
+beginDeploy "############# GIT ALIASES #############"
+sh -c 'curl -s https://raw.githubusercontent.com/maxyermayank/developer-mac-setup/master/.git_aliases >> ~/.git_aliases'
+source ~/.git_aliases
 
+beginDeploy "############# DOCKER ALIASES #############"
+sh -c 'curl -s https://raw.githubusercontent.com/maxyermayank/developer-mac-setup/master/.docker_aliases >> ~/.docker_aliases'
+source ~/.docker_aliases
 
-## Docker Swarm Aliases
-alias dstr='docker stack rm'
-alias dsls='docker service ls'
-alias dsp='docker service ps'
-alias dsl='docker service logs'
-alias dsr='docker service rm'
-" >> ~/.bash_profile
+beginDeploy "############# K8s ALIASES #############"
+sh -c 'curl -s https://raw.githubusercontent.com/maxyermayank/developer-mac-setup/master/.kubectl_aliases >> ~/.kubectl_aliases'
+source ~/.kubectl_aliases
 
-
-## Source Bash Profile
+beginDeploy "############# SETUP BASH PROFILE #############"
 source ~/.bash_profile
+
+runtime=$((($(date +%s)-$start)/60))
+beginDeploy "############# Total Setup Time ############# $runtime Minutes"
